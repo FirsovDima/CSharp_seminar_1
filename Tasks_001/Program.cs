66e8bc5c8861d3a2348d77732e06fb0e6ca59269
Firsov <dimafirsov88@gmail.com>
2023-10-24 23:28:11 +0300

    дз_1

--- Tasks_001/Program.cs
+++ Tasks_001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1:Напишите программу которая на вход принимает два числа и выдает какое число больше, а какое меньше
+// a=5; b=7; max = 7
+// a=2; b=10; max = 10
+// a=-9; b=-3; max = -3
+
+System.Console.WriteLine("В ведите первое число");
+    int a  = Convert.ToInt32 (Console.ReadLine ());
+System.Console.WriteLine("В ведите  второе число");
+    int b  = Convert.ToInt32 (Console.ReadLine ());
+    if ( a>b )
+    {
+        System.Console.WriteLine("первое число max");
+    }
+    else
+    {
+        System.Console.WriteLine("второе число max");
+    }
