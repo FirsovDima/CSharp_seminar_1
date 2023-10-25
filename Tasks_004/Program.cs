8793e1aedf9e067ca4c7c919f62d44fb06cef548
Firsov <dimafirsov88@gmail.com>
2023-10-25 22:41:05 +0300

    задача 8

--- Tasks_004/Program.cs
+++ Tasks_004/Program.cs
@@ -1,4 +1,7 @@
-﻿System.Console.WriteLine("Введите  число");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+//5->2, 4
+//8->2, 4, 6, 8
+System.Console.WriteLine("Введите  число");
 int a  = Convert.ToInt32 (Console.ReadLine ());
 for (int b = 2; b <= a; b+=2)  
 {
