995a8b2ff7502ee8c0c5c92c0428986da735df63
Firsov <dimafirsov88@gmail.com>
2023-10-25 22:09:39 +0300

    задача 6

--- Tasks_003/Program.cs
+++ Tasks_003/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите  число");
+int a  = Convert.ToInt32 (Console.ReadLine ());
+if( a % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+    }
+else
+{
+System.Console.WriteLine("Число не четное");
+}
